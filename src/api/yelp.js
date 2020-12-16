64dc6a9b71e5423dc9af51215012df2def1dffb1
Esteban Hernandez <contact@mksmdesign.com>
2020-12-15 21:57:14 -0700

    Updated cached gitignore file

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
-  headers: {
-    Authorization: 'Bearer Tg87LiLneDAIK1eNooKGWzj9dx3-MayuIz1r9yDidP3nQlUBqjByQzaK4gRzAdLUQ14vprOwhAfR1gFNeP4DISAzzCv4IFQM9v_VcPbOeDj-aet0o5MXoiWs3pLZX3Yx'
-  }
-})
